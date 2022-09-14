c8edc28e8dd19beddfad0f938416f95c03f8abec
Irfan <irfan17h@gmail.com>
2022-09-13 22:29:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     #'<a href = "./predict?text=exampletext"> ./predict?text=exampletext</a>'
 
 
-@app.route("/predict", methods = ['GET','POST'])
+@app.route("/predict", methods = ['POST'])
 def predict():
     """Takes a POST request with a key of \"text\" and the text to be classified."""
     age = request.form.get("age")
@@ -23,7 +23,7 @@ def predict():
     # get data in correct ofrmat and then do your predict and stuff
     classifier_1 = load("classifier_1.pkl")
     prediction = classifier_1.predict([[age, sex, breed]])[0]
-    if prediction == int(prediction)== 0 :
+    if prediction == int(prediction)== 0:
         return render_template("index.html", prediction='Unavailable')
     else:
         return render_template("index.html", prediction='Available')
